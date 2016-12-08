dd8f71c6ac0640ae47024f6654b7f32cff16e705
SergeyDerko <sergeyderko1986@yandex.ru>
2016-12-08 16:53:36 +0200

    Math Gipotenuza

---
+++ Gipotenuza/Gipotenuza/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gipotenuza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Веедите длину катета А");
+            double A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите катет B");
+            double B = double.Parse(Console.ReadLine());
+            A *= A;
+            B *= B;
+            double C = Math.Sqrt(A + B);
+            Console.WriteLine("Гипотенуза равна - {0}", C);
+            Console.Read();
+
+        }
+    }
+}
